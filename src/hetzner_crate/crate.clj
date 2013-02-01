d8be0121f3a5019d773b38e2388cd1e0e5c9e97e
Patrik Sundberg <patrik.sundberg@gmail.com>
2013-02-01 17:53:33 +0000

    Update to new pallet API, 0.8.0-alpha.8 style.
    Love the new API compared to the monadic one!

--- src/hetzner_crate/crate.clj
+++ src/hetzner_crate/crate.clj
@@ -1,52 +1,49 @@
 (ns hetzner-crate.crate
   "Crate with functions for Hetzner servers"
-  (:require
-   [pallet.actions :as actions]
-   [pallet.environment :as env]
-   [pallet.crate :as crate]
-   [pallet.crate.etc-hosts :as etc-hosts]
-   [pallet.crate.ssh-key :as ssh-key]
-   [pallet.crate.automated-admin-user :as admin-user])
-  (:use [pallet.crate :only [def-plan-fn]]
-        [clojure.algo.monads :only [m-when m-when-not m-result]]))
+  (:require [pallet.actions :as actions]
+            [pallet.crate :as crate]
+            [pallet.crate.automated-admin-user :as admin-user]
+            [pallet.crate.etc-hosts :as etc-hosts]
+            [pallet.crate.ssh-key :as ssh-key]
+            [pallet.environment :as env]
+            [pallet.crate :refer [defplan]]))
 
-(def-plan-fn set-timezone
+(defplan set-timezone
   "Set timezone of host"
   [timezone]
-  []
   (actions/remote-file "/etc/timezone" :content timezone)
   (actions/exec-checked-script
    "update time zone"
    (dpkg-reconfigure --frontend noninteractive tzdata)))
 
-(def-plan-fn initial-setup
+(defplan initial-setup
   []
-  [node-hostname crate/target-name
-   host-config (env/get-environment [:host-config node-hostname])
-   admin-username (m-result (get-in host-config [:admin-user :username]))
-   admin-ssh-public-key-path (m-result (get-in host-config [:admin-user :ssh-public-key-path]))
-   timezone (m-result (:timezone host-config))
-   rootpass (m-result (:rootpass host-config))
-   ip (m-result (:ip host-config))
-   private-ip (m-result (:private-ip host-config))
-   private-hostname (m-result (:private-hostname host-config))]
+  (let [node-hostname crate/target-name
+        host-config (env/get-environment [:host-config node-hostname])
+        admin-username (get-in host-config [:admin-user :username])
+        admin-ssh-public-key-path (get-in host-config [:admin-user :ssh-public-key-path])
+        timezone (:timezone host-config)
+        rootpass (:rootpass host-config)
+        ip (:ip host-config)
+        private-ip (:private-ip host-config)
+        private-hostname (:private-hostname host-config)]
 
-  (set-timezone timezone)
+    (set-timezone timezone)
 
-  ;; /etc/hosts and hostname
-  (etc-hosts/host ip node-hostname)
-  (if-not (nil? private-ip)
-    (etc-hosts/host private-ip private-hostname))
-  etc-hosts/hosts
-  (etc-hosts/set-hostname)
+    ;; /etc/hosts and hostname
+    (etc-hosts/host ip node-hostname)
+    (if-not (nil? private-ip)
+      (etc-hosts/host private-ip private-hostname))
+    etc-hosts/hosts
+    (etc-hosts/set-hostname)
 
-  ;; rootpass
-  (actions/exec-checked-script
-   "change root passwd"
-   (pipe (echo ~(format "root:%s" rootpass))
-         (chpasswd)))
+    ;; rootpass
+    (actions/exec-checked-script
+     "change root passwd"
+     (pipe (echo ~(format "root:%s" rootpass))
+           (chpasswd)))
 
-  ;; admin user
-  (if (= admin-username "root")
-    (ssh-key/authorize-key "root" (slurp admin-ssh-public-key-path))
-    (admin-user/automated-admin-user admin-username admin-ssh-public-key-path)))
+    ;; admin user
+    (if (= admin-username "root")
+      (ssh-key/authorize-key "root" (slurp admin-ssh-public-key-path))
+      (admin-user/automated-admin-user admin-username admin-ssh-public-key-path))))
